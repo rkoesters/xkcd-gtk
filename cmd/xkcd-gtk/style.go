116fd063f146c7bfb61795807856d64d2c3b294d
Ryan Koesters <rmkoesters@gmail.com>
2018-10-08 23:26:14 -0500

    Move StyleUpdated from window.go to style.go.

--- cmd/xkcd-gtk/style.go
+++ cmd/xkcd-gtk/style.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gotk3/gotk3/gdk"
 	"github.com/gotk3/gotk3/gtk"
 	"log"
+	"os"
 )
 
 const css = `
@@ -14,6 +16,16 @@ const css = `
 	background-color: #ffffff;
 }
 `
+
+var (
+	// largeToolbarThemes is the list of gtk themes for which we should use
+	// large toolbar buttons.
+	largeToolbarThemes = []string{"elementary", "win32"}
+
+	// symbolicIconThemes is the list of gtk themes for which we should use
+	// symbolic icons.
+	symbolicIconThemes = []string{"Adwaita"}
+)
 
 // LoadCSS provides the application's custom CSS to GTK.
 func (a *Application) LoadCSS() {
@@ -31,4 +43,81 @@ func (a *Application) LoadCSS() {
 	}
 
 	gtk.AddProviderForScreen(screen, provider, gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+}
+
+// StyleUpdated is called when the style of our gtk window is updated.
+func (w *Window) StyleUpdated() {
+	// First, lets find out what GTK theme we are using.
+	themeName := os.Getenv("GTK_THEME")
+	if themeName == "" {
+		// The theme is not being set by the environment, so lets ask
+		// GTK what theme it is going to use.
+		settings, err := gtk.SettingsGetDefault()
+		if err != nil {
+			log.Print(err)
+		} else {
+			// settings.GetProperty returns an interface{}, we will convert
+			// it to a string in a moment.
+			themeNameIface, err := settings.GetProperty("gtk-theme-name")
+			if err != nil {
+				log.Print(err)
+			} else {
+				themeNameStr, ok := themeNameIface.(string)
+				if ok {
+					themeName = themeNameStr
+				}
+			}
+		}
+	}
+
+	// The default size for our headerbar buttons is small.
+	headerBarIconSize := gtk.ICON_SIZE_SMALL_TOOLBAR
+	for _, largeToolbarTheme := range largeToolbarThemes {
+		if themeName == largeToolbarTheme {
+			headerBarIconSize = gtk.ICON_SIZE_LARGE_TOOLBAR
+		}
+	}
+
+	// Should we use symbolic icons?
+	useSymbolicIcons := false
+	for _, symbolicIconTheme := range symbolicIconThemes {
+		if themeName == symbolicIconTheme {
+			useSymbolicIcons = true
+		}
+	}
+	// we will call icon() to automatically add -symbolic if needed.
+	icon := func(s string) string {
+		if useSymbolicIcons {
+			return fmt.Sprint(s, "-symbolic")
+		}
+		return s
+	}
+
+	nextImg, err := gtk.ImageNewFromIconName(icon("go-next"), headerBarIconSize)
+	if err != nil {
+		log.Print(err)
+	} else {
+		w.next.SetImage(nextImg)
+	}
+
+	previousImg, err := gtk.ImageNewFromIconName(icon("go-previous"), headerBarIconSize)
+	if err != nil {
+		log.Print(err)
+	} else {
+		w.previous.SetImage(previousImg)
+	}
+
+	searchImg, err := gtk.ImageNewFromIconName(icon("edit-find"), headerBarIconSize)
+	if err != nil {
+		log.Print(err)
+	} else {
+		w.search.SetImage(searchImg)
+	}
+
+	menuImg, err := gtk.ImageNewFromIconName(icon("open-menu"), headerBarIconSize)
+	if err != nil {
+		log.Print(err)
+	} else {
+		w.menu.SetImage(menuImg)
+	}
 }
