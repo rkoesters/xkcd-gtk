66eb4df81f4d88e72da465a2eb57b1bbd408c370
Ryan Koesters <rmkoesters@gmail.com>
2018-10-12 15:02:01 -0500

    Use boltdb for comic metadata cache.

--- cmd/xkcd-gtk/main.go
+++ cmd/xkcd-gtk/main.go
@@ -16,21 +16,33 @@ func main() {
 	// Tell the go runtime to use as many CPUs as are available.
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	// Make sure our random number generator is seeded.
+	// Make sure the random number generator is seeded.
 	rand.Seed(time.Now().Unix())
+
+	// Initialize the comic cache.
+	err := initComicCache()
+	if err != nil {
+		log.Fatalf("failed to initialize comic cache: %v", err)
+	}
 
 	// Let glib and gtk know who we are.
 	glib.SetApplicationName(appName)
 	gtk.WindowSetDefaultIconName(appID)
 
-	// Create our application.
+	// Create the application.
 	app, err := NewApplication()
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Tell glib that this is our process's main application.
+	// Tell glib that this is the process's main application.
 	app.application.SetDefault()
 
-	// Run the event loop; exit if it returns.
-	os.Exit(app.application.Run(os.Args))
+	// Run the event loop.
+	ret := app.application.Run(os.Args)
+
+	// Close the comic cache.
+	closeComicCache()
+
+	// Exit with the status code given by gtk.
+	os.Exit(ret)
 }
