283b61fee8dd3f18536596dd0c9907bfe8ec305c
Ryan Koesters <rmkoesters@gmail.com>
2018-10-21 00:23:37 -0500

    Better support for getting version number in tarball.

--- tools/app-version.sh
+++ tools/app-version.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 git describe --always --tags --dirty 2>/dev/null ||
 dpkg-parsechangelog -S Version 2>/dev/null ||
+basename $PWD | grep -o '[0-9][0-9]*\.[0-9][0-9]*\..*' ||
 echo "unknown"
