e7671884829db50fa63f83874a104c183858d486
Ryan Koesters <rmkoesters@gmail.com>
2018-10-31 21:05:40 -0500

    Update package-for-windows.sh.

--- tools/package-for-windows.sh
+++ tools/package-for-windows.sh
@@ -17,30 +17,48 @@ install -t "$tmp_target" "$exe"
 
 echo "Copying DLLs..."
 ldd "$exe" |
+grep "=>" |
 grep "$MINGW_PREFIX" |
 cut -d ' ' -f 3 |
 sort |
 uniq |
 xargs install -t "$tmp_target"
 
-echo "Copying pixbuf loaders..."
+echo "Copying gdk-pixbuf loaders..."
 mkdir -p "$tmp_target/lib/gdk-pixbuf-2.0/2.10.0/loaders"
 install -t "$tmp_target/lib/gdk-pixbuf-2.0/2.10.0/loaders" "$MINGW_PREFIX/lib/gdk-pixbuf-2.0/2.10.0/loaders"/*.dll
 cp "$MINGW_PREFIX/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache" "$tmp_target/lib/gdk-pixbuf-2.0/2.10.0"
 
+echo "Copying gdk-pixbuf DLLs..."
+ldd "$MINGW_PREFIX/lib/gdk-pixbuf-2.0/2.10.0/loaders"/*.dll |
+grep "=>" |
+grep "$MINGW_PREFIX" |
+cut -d ' ' -f 3 |
+sort |
+uniq |
+xargs install -t "$tmp_target"
+
 echo "Copying icons..."
 mkdir -p "$tmp_target/share/icons/"
 cp -R "$MINGW_PREFIX/share/icons/hicolor" "$tmp_target/share/icons/"
-cp -R "$MINGW_PREFIX/share/icons/Adwaita"/*/ "$tmp_target/share/icons/hicolor"
 cp "$icon" "$tmp_target/share/icons/hicolor/scalable/apps/"
 gtk-update-icon-cache-3.0 -f "$tmp_target/share/icons/hicolor"
+cp -R "$MINGW_PREFIX/share/icons/Adwaita" "$tmp_target/share/icons/"
+gtk-update-icon-cache-3.0 -f "$tmp_target/share/icons/Adwaita"
 
 echo "Creating gtk settings..."
 mkdir -p "$tmp_target/etc/gtk-3.0"
 {
 	echo "[Settings]"
-	echo "gtk-theme-name=win32"
-	echo "gtk-icon-theme-name=hicolor"
+	echo "gtk-theme-name = win32"
+	echo "gtk-icon-theme-name = Adwaita"
+	echo ""
+	echo "gtk-dialogs-use-header = true"
+	echo ""
+	echo "gtk-xft-antialias = 1"
+	echo "gtk-xft-hinting = 1"
+	echo "gtk-xft-hintstyle = hintfull"
+	echo "gtk-xft-rgba = rgb"
 } >"$tmp_target/etc/gtk-3.0/settings.ini"
 
 echo "Creating zip package..."
