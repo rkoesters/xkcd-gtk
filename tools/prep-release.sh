559bffa74dfade6b5d858e7e8f9db78c1b15a8d2
Ryan Koesters <rmkoesters@gmail.com>
2018-11-02 12:49:34 -0500

    Update tools/prep-release.sh.

--- tools/prep-release.sh
+++ tools/prep-release.sh
@@ -1,3 +1,11 @@
 #!/bin/sh -eu
-appstream-util validate-relax data/*.appdata.xml
-git describe --exact-match --tags HEAD
+appdata_xml=$(echo data/*.appdata.xml)
+
+appstream-util validate-relax "$appdata_xml"
+
+version=$(git describe --exact-match --tags HEAD)
+
+if ! grep -q "<release version=\"$version\"" "$appdata_xml"; then
+	echo "version $version not found in appdata changelog"
+	exit 1
+fi
