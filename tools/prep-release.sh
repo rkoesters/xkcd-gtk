0f1f6871d5c9c8d3348ca04513ddebd172f0722a
Ryan Koesters <rmkoesters@gmail.com>
2018-11-06 12:03:55 -0600

    Update tools/prep-release.sh.

--- tools/prep-release.sh
+++ tools/prep-release.sh
@@ -1,11 +1,16 @@
 #!/bin/sh -eu
 appdata_xml="data/com.github.rkoesters.xkcd-gtk.appdata.xml"
 
+echo "Validating appdata..."
 appstream-util validate-relax "$appdata_xml"
 
+echo "Finding git tag..."
 version=$(git describe --exact-match --tags HEAD)
 
+echo "Checking for git tag in appdata changelog..."
 if ! grep -q "<release version=\"$version\"" "$appdata_xml"; then
 	echo "version $version not found in appdata changelog"
 	exit 1
 fi
+
+echo "Checks passed!"
