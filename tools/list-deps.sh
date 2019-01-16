3a0734f1be98063a2192859f51d60a8668f3e8f0
Ryan Koesters <rmkoesters@gmail.com>
2019-01-16 13:42:20 -0600

    Tweak list-deps.sh to only show repos instead of each package.

--- tools/list-deps.sh
+++ tools/list-deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 go list -f '{{ join .Deps "\n" }}' "$@" | # List all of our dependencies.
-grep '\.' | # Filter out all dependencies that don't appear to be URLs.
+grep '^[^/]\+\.[^/]\+/[^/]\+/[^/]\+$' | # Filter to repos (e.g. github.com/*/*).
 sort | # Sort dependencies so 'uniq' can work properly.
 uniq # Filter out repeated dependencies.
