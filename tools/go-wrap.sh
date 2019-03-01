a50e3da5ffd97c96e1aee1694579ddc277441abd
Ryan Koesters <rmkoesters@gmail.com>
2019-02-28 18:28:37 -0600

    Update go-wrap.sh.

--- tools/go-wrap.sh
+++ tools/go-wrap.sh
@@ -1,11 +1,12 @@
 #!/bin/sh -eu
-# simple script to wrap plain text file as go const string
+# simple script to wrap a plain text file as go const string
 
 if [ $# != 1 ]; then
 	echo "usage: $0 FILE"
 	exit 1
 fi
 
-varName="$(basename "$1" '.in' | tr '.-' '_')"
+constName="$(basename "$1" | tr '.-' '_')"
+constValue="$(cat "$1")"
 
-printf 'package main\n\nconst %s = `%s`\n' "$varName" "$(cat "$1")"
+printf 'package main\n\nconst %s = `%s\n`\n' "$constName" "$constValue"
