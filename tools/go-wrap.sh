ff7fbf5605f64c234797592d9e81a9a3abc05e30
Ryan Koesters <rmkoesters@gmail.com>
2019-05-04 00:33:39 -0700

    Rewrite tools/go-wrap.sh.

--- tools/go-wrap.sh
+++ tools/go-wrap.sh
@@ -1,12 +1,18 @@
 #!/bin/sh -eu
-# simple script to wrap a plain text file as go const string
+# Simple script to wrap a plain text file as go const string.
 
 if [ $# != 1 ]; then
 	echo "usage: $0 FILE"
 	exit 1
 fi
 
-constName="$(basename "$1" | tr '.-' '_' | sed -e 's/_\([^_]*\)$/\U\1/g' -e 's/_\([a-z]\)/\U\1/g')"
-constValue="$(cat "$1")"
+printf 'package main\n\nconst '
 
-printf 'package main\n\nconst %s = `%s\n`\n' "$constName" "$constValue"
+basename "$1" | tr '.-' '_' | tr -d '\n' |
+sed -e 's/_\([^_]*\)$/\U\1/g' -e 's/_\([a-z]\)/\U\1/g'
+
+printf ' = `'
+
+cat "$1"
+
+printf '`\n'
