9de1504fc0e4531cdf52f9001044f65e80536af2
Ryan Koesters <rmkoesters@gmail.com>
2019-05-03 17:55:33 -0700

    Add tools/list-dep-versions.sh.

---
+++ tools/list-dep-versions.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -eu
+# Find and print versions (or commits) of the dependencies of the given Go
+# packages.
+
+get_package_version () {
+	git -C "$1" describe --always --tags --dirty
+}
+
+print_package_version () {
+	printf '%s\t%s\n' "$1" "$(get_package_version "$1")"
+}
+
+# Run all our tools/* before we change directory.
+deps="$(tools/list-deps.sh "$@")"
+
+# Change directory to $GOPATH/src so that package names work as relative
+# paths to the respective package.
+cd "$(go env GOPATH)/src"
+
+for package in $deps; do
+	print_package_version "$package"
+done
