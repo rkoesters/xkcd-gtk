0da18e1dc154cbba748b94085cc64430687aa40b
Ryan Koesters <rmkoesters@gmail.com>
2019-05-03 19:55:09 -0700

    Simplify tools/list-deps.sh.

--- tools/list-deps.sh
+++ tools/list-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 go list -f '{{ join .Deps "\n" }}' "$@" | # List all of our dependencies.
 grep '\.' | # Filter out all dependencies that don't appear to be URLs.
-sed 's#^\([^/]\+\.[^/]\+/[^/]\+/[^/]\+\).*$#\1#g' | # Trim URLs to repo root (e.g. github.com/*/*).
+sed 's#^\([^/]\+/[^/]\+/[^/]\+\).*$#\1#g' | # Trim URLs to repo root (e.g. github.com/user/repo).
 sort | # Sort dependencies so 'uniq' can work properly.
 uniq # Filter out repeated dependencies.
