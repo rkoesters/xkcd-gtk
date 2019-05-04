50aacc5431c391e47c7e3dff296ce8521caef1c5
Ryan Koesters <rmkoesters@gmail.com>
2019-05-03 19:44:13 -0700

    Fix list-deps.sh not printing root of nested packages.

--- tools/list-deps.sh
+++ tools/list-deps.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 go list -f '{{ join .Deps "\n" }}' "$@" | # List all of our dependencies.
-grep '^[^/]\+\.[^/]\+/[^/]\+/[^/]\+$' | # Filter to repos (e.g. github.com/*/*).
+grep '\.' | # Filter out all dependencies that don't appear to be URLs.
+sed 's#^\([^/]\+\.[^/]\+/[^/]\+/[^/]\+\).*$#\1#g' | # Trim URLs to repo root (e.g. github.com/*/*).
 sort | # Sort dependencies so 'uniq' can work properly.
 uniq # Filter out repeated dependencies.
