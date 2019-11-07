d02f7a31f2258a5331a31b19a59b1ddddc9a71d9
Ryan Koesters <rmkoesters@gmail.com>
2019-11-06 18:37:31 -0800

    Add more logging to tools/test-install.sh.

--- tools/test-install.sh
+++ tools/test-install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -eu
 
+echo "mkdir out"
 mkdir out
 make prefix=out install
 make prefix=out uninstall
+echo "rmdir out/..."
 rmdir out/*/*/*/*/* out/*/*/*/* out/*/*/* out/*/* out/* out
