196a123e48812ee749a3ea0d445ec145131496d7
Ryan Koesters <rmkoesters@gmail.com>
2020-02-01 17:52:18 -0800

    Filter our internal packages from tools/list-(deps|imports).sh.

--- tools/list-deps.sh
+++ tools/list-deps.sh
@@ -2,5 +2,6 @@
 go list -f '{{ join .Deps "\n" }}' "$@" | # List all of our dependencies.
 grep '\.' | # Filter out all dependencies that don't appear to be URLs.
 sed 's#^\([^/]\+/[^/]\+/[^/]\+\).*$#\1#g' | # Trim URLs to repo root (e.g. github.com/user/repo).
+grep -v 'github.com/rkoesters/xkcd-gtk' | # Filter out ourselves.
 sort | # Sort dependencies so 'uniq' can work properly.
 uniq # Filter out repeated dependencies.
--- tools/list-imports.sh
+++ tools/list-imports.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 go list -f '{{ join .Imports "\n" }}' "$@" | # List all of our imports.
 grep '\.' | # Filter out all imports that don't appear to be URLs.
+grep -v 'github.com/rkoesters/xkcd-gtk' | # Filter out ourselves.
 sort | # Sort imports so 'uniq' can work properly.
 uniq # Filter out repeated imports.
