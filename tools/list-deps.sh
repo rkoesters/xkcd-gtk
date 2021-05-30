d610a7edc2961fddb18441fab69383e3b138beef
Ryan Koesters <rmkoesters@gmail.com>
2021-05-30 16:58:46 -0500

    Add a rewritten tools/list-deps.sh.

---
+++ tools/list-deps.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Find and print versions of the dependencies of all go.mod files under the
+# current directory. Run `make vendor` before running to get versions of all
+# dependencies.
+set -eu
+
+cat $(find . -name 'go.mod' -type f) |
+grep '	' |
+tr -d '\t' |
+cut -d ' ' -f 1 |
+sort |
+uniq
