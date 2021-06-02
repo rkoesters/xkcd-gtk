53a6698a3f298e638f6245ddeea88c12adfb86f6
Ryan Koesters <rmkoesters@gmail.com>
2021-06-02 01:06:27 -0500

    Update tools/fill-pot-header.sh.

--- tools/fill-pot-header.sh
+++ tools/fill-pot-header.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sed \
 	-e "s/SOME DESCRIPTIVE TITLE/Translations for Comic Sticks (xkcd-gtk)/" \
-	-e "s/YEAR THE PACKAGE'S COPYRIGHT HOLDER/2015-2020 Ryan Koesters/" \
+	-e "s/YEAR THE PACKAGE'S COPYRIGHT HOLDER/2015-2021 Ryan Koesters/" \
 	-e "s/CHARSET/UTF-8/"
