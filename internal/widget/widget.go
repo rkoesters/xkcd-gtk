ac3c853859b99bae8abd584a7f30d9679ff6dcdc
Ryan Koesters <rmkoesters@gmail.com>
2021-06-02 22:01:25 -0500

    Add widget.Widget interface.

--- internal/widget/widget.go
+++ internal/widget/widget.go
@@ -1,2 +1,11 @@
 // Package widget provides custom GTK+ widgets.
 package widget
+
+import (
+	"github.com/gotk3/gotk3/gtk"
+)
+
+type Widget interface {
+	// Return the Widget's top-level gtk.Widget.
+	IWidget() gtk.IWidget
+}
