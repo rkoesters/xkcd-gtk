bf8eb6b6c79411ff8a07075ed540539aa1567cba
Ryan Koesters <rmkoesters@gmail.com>
2021-06-06 13:28:00 -0700

    Add paths.Ensure*Dir() methods for creating app directories.

--- internal/paths/builder.go
+++ internal/paths/builder.go
@@ -6,6 +6,8 @@ import (
 	"path/filepath"
 )
 
+const defaultDirMode = 0755
+
 type builder struct {
 	appID string
 }
@@ -15,14 +17,32 @@ func (b builder) CacheDir() string {
 	return filepath.Join(basedir.CacheHome, b.appID)
 }
 
+// EnsureCacheDir creates the CacheDir() directory, if it doesn't exist. It does
+// not return an error if it already exists.
+func (b builder) EnsureCacheDir() error {
+	return os.MkdirAll(b.CacheDir(), defaultDirMode)
+}
+
 // ConfigDir returns the path to our app's user configuration directory.
 func (b builder) ConfigDir() string {
 	return filepath.Join(basedir.ConfigHome, b.appID)
 }
 
+// EnsureConfigDir creates the ConfigDir() directory, if it doesn't exist. It
+// does not return an error if it already exists.
+func (b builder) EnsureConfigDir() error {
+	return os.MkdirAll(b.ConfigDir(), defaultDirMode)
+}
+
 // DataDir returns the path to our app's user data directory.
 func (b builder) DataDir() string {
 	return filepath.Join(basedir.DataHome, b.appID)
+}
+
+// EnsureDataDir creates the DataDir() directory, if it doesn't exist. It does
+// not return an error if it already exists.
+func (b builder) EnsureDataDir() error {
+	return os.MkdirAll(b.DataDir(), defaultDirMode)
 }
 
 // LocaleDir returns the path to the system locale directory.
--- internal/paths/paths.go
+++ internal/paths/paths.go
@@ -16,14 +16,32 @@ func CacheDir() string {
 	return b.CacheDir()
 }
 
+// EnsureCacheDir creates the CacheDir() directory, if it doesn't exist. It does
+// not return an error if it already exists.
+func EnsureCacheDir() error {
+	return b.EnsureCacheDir()
+}
+
 // ConfigDir returns the path to our app's user configuration directory.
 func ConfigDir() string {
 	return b.ConfigDir()
 }
 
+// EnsureConfigDir creates the ConfigDir() directory, if it doesn't exist. It
+// does not return an error if it already exists.
+func EnsureConfigDir() error {
+	return b.EnsureConfigDir()
+}
+
 // DataDir returns the path to our app's user data directory.
 func DataDir() string {
 	return b.DataDir()
+}
+
+// EnsureDataDir creates the DataDir() directory, if it doesn't exist. It does
+// not return an error if it already exists.
+func EnsureDataDir() error {
+	return b.EnsureDataDir()
 }
 
 // LocaleDir returns the path to the system locale directory.
