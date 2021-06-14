c0d282004b870714c23c14f3b4f2d926a074b674
Ryan Koesters <rmkoesters@gmail.com>
2021-06-13 23:34:37 -0700

    Make tools/list-deps.sh more accurate.

--- tools/list-deps.sh
+++ tools/list-deps.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-# Find and print the dependencies of this go module.
+# Find and print the dependencies of the given go packages.
 set -eu
 
-go mod graph |
-cut -d ' ' -f 2 |
+go list -f '{{ join .Deps "\n" }}' "$@" |
+grep -v -e '^internal/' -e '^vendor/' -e '^github.com/rkoesters/xkcd-gtk' |
+grep '^.*\..*/' |
+sed -e 's#\([^/]*/[^/]*/[^/]*\).*#\1#g' |
 sort |
 uniq
