a51d7082ed04ee77405684391d9e779f7b7be5a2
Ryan Koesters <rmkoesters@gmail.com>
2021-06-13 22:43:37 -0700

    Add script to help maintain deps.

---
+++ tools/gen-deps-yml.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -eu
+
+IFS='
+'
+for dep in $(tools/list-deps.sh); do
+	name=$(echo "${dep:?}" | cut -d '@' -f 1)
+	version=$(echo "${dep:?}" | cut -d '@' -f 2)
+
+	echo "      - type: git"
+	echo "        url: https://${name:?}.git"
+	echo "        tag: ${version:?}"
+	echo "        dest: vendor/${name:?}"
+	echo
+done
