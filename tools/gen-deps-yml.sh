b3509031510b767754cb16f1dc940cdccd65772c
Ryan Koesters <rmkoesters@gmail.com>
2021-07-17 02:03:07 -0700

    Improve tools/gen-deps-yml.sh's output.
    * Translate remotes of repos that are known to use vanity import paths.
    
    * Convert go mod pseudo-versions (e.g. v0.0.0-.*) into commits.
    
    * Support repos that use `/vN` suffix (e.g. `/v12`).

--- tools/gen-deps-yml.sh
+++ tools/gen-deps-yml.sh
@@ -1,15 +1,36 @@
 #!/bin/sh
 set -eu
 
+name_to_remote () {
+	printf 'https://'
+	printf '%s' "${1:?}" |
+	sed \
+		-e 's#/v[0-9][0-9]*$##g' \
+		-e 's#go.etcd.io/#github.com/etcd-io/#g' \
+		-e 's#golang.org/x/#go.googlesource.com/#g' \
+	# This comment ensures printf does not become an argument to sed.
+	printf '.git\n'
+}
+
 IFS='
 '
 for dep in $(tools/list-mod-deps.sh); do
 	name=$(echo "${dep:?}" | cut -d '@' -f 1)
 	version=$(echo "${dep:?}" | cut -d '@' -f 2)
+	remote="$(name_to_remote "${name:?}")"
 
-	echo "      - type: git"
-	echo "        url: https://${name:?}.git"
-	echo "        tag: ${version:?}"
-	echo "        dest: vendor/${name:?}"
 	echo
+	echo "      - type: git"
+	echo "        url: ${remote:?}"
+	case ${version:?} in
+		v0.0.0-*)
+			commit="$(echo "${version:?}" | cut -d '-' -f 3)"
+			echo "        # ${version:?}"
+			echo "        commit: ${commit:?}"
+			;;
+		*)
+			echo "        tag: ${version:?}"
+			;;
+	esac
+	echo "        dest: vendor/${name:?}"
 done
