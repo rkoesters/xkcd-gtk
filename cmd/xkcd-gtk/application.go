616e251503bb2179751d41156c9059626fe744c6
Ryan Koesters <rmkoesters@gmail.com>
2021-07-17 01:09:21 -0700

    Move NewAboutDialog into the widget package.

--- cmd/xkcd-gtk/application.go
+++ cmd/xkcd-gtk/application.go
@@ -212,6 +212,39 @@ func (app *Application) ShowShortcuts() {
 	shortcutsWindow.Present()
 }
 
+var aboutDialog *gtk.AboutDialog
+
+// ShowAbout shows our application info to the user.
+func (app *Application) ShowAbout() {
+	var err error
+
+	if aboutDialog == nil {
+		aboutDialog, err = widget.NewAboutDialog(appID, appName, appVersion)
+		if err != nil {
+			log.Print("error creating about dialog: ", err)
+			return
+		}
+
+		// We want to keep the about dialog around in case we want to
+		// show it again.
+		aboutDialog.HideOnDelete()
+		aboutDialog.Connect("response", aboutDialog.Hide)
+		aboutDialog.Connect("hide", func() {
+			app.application.RemoveWindow(&aboutDialog.Window)
+		})
+	}
+
+	// Set our parent window as the active window, but avoid accidentally
+	// setting ourself as the parent window.
+	win := app.application.GetActiveWindow()
+	if win.Native() != aboutDialog.Native() {
+		aboutDialog.SetTransientFor(win)
+	}
+
+	app.application.AddWindow(&aboutDialog.Window)
+	aboutDialog.Present()
+}
+
 const (
 	whatIfLink = "https://what-if.xkcd.com/"
 	blogLink   = "https://blog.xkcd.com/"
---
+++ internal/widget/about-dialog.go
@@ -0,0 +1,24 @@
+package widget
+
+import (
+	"github.com/gotk3/gotk3/gtk"
+)
+
+// NewAboutDialog creates our about dialog.
+func NewAboutDialog(icon, name, version string) (*gtk.AboutDialog, error) {
+	dialog, err := gtk.AboutDialogNew()
+	if err != nil {
+		return nil, err
+	}
+
+	dialog.SetLogoIconName(icon)
+	dialog.SetProgramName(name)
+	dialog.SetVersion(version)
+	dialog.SetComments(l("A simple xkcd viewer written in Go using GTK+3"))
+	dialog.SetWebsite("https://github.com/rkoesters/xkcd-gtk")
+	dialog.SetAuthors([]string{"Ryan Koesters"})
+	dialog.SetCopyright("Copyright © 2015-2021 Ryan Koesters")
+	dialog.SetLicenseType(gtk.LICENSE_GPL_3_0)
+
+	return dialog, nil
+}
