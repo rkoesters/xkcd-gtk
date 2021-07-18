d721fb7e1405d1bb156c8d1b9cfb5863ed3799ba
Ryan Koesters <rmkoesters@gmail.com>
2021-07-18 02:36:41 -0700

    Delete unused tools/list*dep*.sh scripts.

--- tools/list-all-mod-dep-updates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Find and print updates to direct and indirect dependencies of the current
-# module.
-set -eu
-
-# NOTE(SC2046): The word splitting is intentional.
-# shellcheck disable=SC2046
-go list -m -u $(tools/list-all-mod-deps.sh) 2>/dev/null |
-grep -e '\[.*\]'
--- tools/list-mod-dep-updates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Find and print updates to direct dependencies of the current module.
-set -eu
-
-# NOTE(SC2046): The word splitting is intentional.
-# shellcheck disable=SC2046
-go list -m -u $(tools/list-mod-deps.sh) 2>/dev/null |
-grep -e '\[.*\]'
