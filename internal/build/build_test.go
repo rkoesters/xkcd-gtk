32532c477707a9a095d0142c3b60b1848bd06c2d
Ryan Koesters <rmkoesters@gmail.com>
2021-07-17 20:26:41 -0700

    Add tests for internal/build.

---
+++ internal/build/build_test.go
@@ -0,0 +1,56 @@
+package build
+
+import (
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	const data = "version=0.0.0,debug=on"
+	flags := parse(data)
+	if flags == nil {
+		t.Fatal("flags == nil")
+	}
+	if flags["version"] != "0.0.0" || flags["debug"] != "on" {
+		t.Fatalf("parse failure: data='%v' flags='%v'", data, flags)
+	}
+}
+
+func TestParseEmpty(t *testing.T) {
+	const data = ""
+	flags := parse(data)
+	if flags == nil {
+		t.Fatal("flags == nil")
+	}
+	if flags["version"] != "" || flags["debug"] != "" {
+		t.Fatalf("parse failure: data='%v' flags='%v'", data, flags)
+	}
+}
+
+func TestParseBlankKVPair(t *testing.T) {
+	const data = "version=0.0.0,"
+	flags := parse(data)
+	if flags == nil {
+		t.Fatal("flags == nil")
+	}
+	if flags["version"] != "0.0.0" || flags["debug"] != "" {
+		t.Fatalf("parse failure: data='%v' flags='%v'", data, flags)
+	}
+	if flags[""] != "" {
+		t.Fatal("parse failure: blank key=value pair has non-zero value")
+	}
+}
+
+func TestParseBadFormat(t *testing.T) {
+	// Should print warnings, but should not panic.
+	const data = "asdf"
+	flags := parse(data)
+	if flags == nil {
+		t.Fatal("flags == nil")
+	}
+	if flags["version"] != "" || flags["debug"] != "" {
+		t.Fatalf("parse failure: data='%v' flags='%v'", data, flags)
+	}
+	if flags["asdf"] != "" {
+		t.Fatal("parse failure: invalid key=value pair has non-zero value")
+	}
+}
