67af27bcbc40c755638713f78eeffb41052914f7
Ryan Koesters <rmkoesters@gmail.com>
2021-10-22 14:25:53 -0700

    Remove POT-Creation-Date from pot file.
    In order to prevent diffs where the only change is POT-Creation-Date.

--- tools/fill-pot-header.sh
+++ tools/fill-pot-header.sh
@@ -2,4 +2,5 @@
 sed \
   -e "s/SOME DESCRIPTIVE TITLE/Translations for Comic Sticks (xkcd-gtk)/" \
   -e "s/YEAR THE PACKAGE'S COPYRIGHT HOLDER/2015-2021 Ryan Koesters/" \
+  -e '/^"POT-Creation-Date: .*\\n"$/d' \
   -e "s/CHARSET/UTF-8/"
