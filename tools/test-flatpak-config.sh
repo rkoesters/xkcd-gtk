90be584de3239ae35747ffbe0dea77b16b8e61e5
Ryan Koesters <rmkoesters@gmail.com>
2021-10-23 12:52:06 -0700

    Add checks for whether file exists in test-flatpak-config.sh.

--- tools/test-flatpak-config.sh
+++ tools/test-flatpak-config.sh
@@ -10,6 +10,16 @@ while [ $# -gt 0 ]; do
   yml="${1:?}"
   shift
 
+  if [ ! -r "${yml:?}" ]; then
+    echo 'ERROR: cannot read flatpak config:' "${yml:?}" >&2
+    exit 1
+  fi
+
+  if [ ! -f "${yml:?}" ]; then
+    echo 'ERROR: flatpak config is not a regular file:' "${yml:?}" >&2
+    exit 1
+  fi
+
   if [ "$(grep dest: "${yml:?}" | grep -v -e 'dest: go$' |
           sort | uniq -D)" != "" ]; then
     echo "ERROR: duplicate 'dest:' in flatpak config:" >&2
@@ -23,9 +33,8 @@ while [ $# -gt 0 ]; do
     for dup in ${duplicates:?}; do
       grep -n dest: "${yml:?}" | grep "${dup:?}"
     done
-    echo "$0 FAILED"
     exit 1
   fi
 
-  echo "$0 $yml PASSED"
+  echo "$0" "${yml:?}" "PASSED" >&2
 done
