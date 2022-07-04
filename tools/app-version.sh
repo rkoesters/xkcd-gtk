811efeb0ecb5899ff57aa6722d581fa0744df72b
Ryan Koesters <rmkoesters@gmail.com>
2022-07-04 11:42:33 -0500

    Use --match flag in tools/app-version.sh.
    prep-release.sh got this in e42abcfd506f1f0acd8cd103cce0ccbba82def2c,
    app-version.sh should have it, too.

--- tools/app-version.sh
+++ tools/app-version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git describe --tags --dirty 2>/dev/null ||
+git describe --tags --match='[0-9].[0-9]*.[0-9]*' --dirty 2>/dev/null ||
 grep '<release version="' data/com.github.rkoesters.xkcd-gtk.appdata.xml |
   head -n 1 | cut -d '"' -f 2 ||
 echo "unknown"
