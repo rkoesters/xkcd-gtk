44f96b21622e648a6a035a31ea51461ded1910b6
Ryan Koesters <rmkoesters@gmail.com>
2022-07-07 22:13:22 -0700

    Make openURL return an error, even though nothing uses it.

--- internal/widget/open-url.go
+++ internal/widget/open-url.go
@@ -5,9 +5,14 @@ import (
 	"github.com/rkoesters/xkcd-gtk/internal/log"
 )
 
-func openURL(url string) {
+// openURL opens the given URL in the user's web browser. The function logs
+// errors in addition to returning them, so errors can be ignored if that is all
+// that would be done with the returned error.
+func openURL(url string) error {
 	err := xdg.Open(url)
 	if err != nil {
 		log.Print("error opening ", url, " in web browser: ", err)
+		return err
 	}
+	return nil
 }
