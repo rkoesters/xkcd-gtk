5d3dbb854622ec7e689fafc419b07acb4699e55b
Ryan Koesters <rmkoesters@gmail.com>
2022-07-13 00:34:50 -0700

    Check the exit status of list-pkg-deps.sh in list-app-deps.sh.

--- tools/list-app-deps.sh
+++ tools/list-app-deps.sh
@@ -4,6 +4,10 @@
 set -eu
 
 pkg_deps="$(tools/list-pkg-deps.sh github.com/rkoesters/xkcd-gtk/cmd/xkcd-gtk)"
+if ! [ $? ]; then
+  echo "$0: error finding dependencies of cmd/xkcd-gtk" >&2
+  exit 1
+fi
 
 go mod graph |
 cut -d ' ' -f 2 |
