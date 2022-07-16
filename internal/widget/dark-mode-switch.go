6f0893dbaf4d248fbb0a1bec3fcb2ec5cfea3afc
Ryan Koesters <rmkoesters@gmail.com>
2022-07-15 23:48:08 -0700

    Fix type for button-release-event in DarkModeSwitch.

--- internal/widget/dark-mode-switch.go
+++ internal/widget/dark-mode-switch.go
@@ -58,13 +58,13 @@ func NewDarkModeSwitch(darkModeGetter func() bool, darkModeSetter func(bool)) (*
 	// the default handlers after our closure, whereas "clicked" always runs
 	// the default handlers first. Running before the default handlers
 	// allows us to prevent the running of the default handlers.
-	dms.label.Connect("button-release-event", func(label *gtk.Label) {
+	dms.label.Connect("button-release-event", func(w gtk.IWidget) {
 		dms.swtch.Activate()
 		// Prevent the default handlers from running so they do not
 		// close the popover menu. The popover menu should remain open
 		// to mimic the behavior of clicking the switch (which would not
 		// close the menu).
-		label.StopEmission("button-release-event")
+		w.ToWidget().StopEmission("button-release-event")
 	})
 	lc, err := dms.label.GetChild()
 	if err != nil {
