12bc1bcc0261922fd872408c0b628d1ecb8891cc
Ryan Koesters <rmkoesters@gmail.com>
2023-01-02 23:33:53 -0600

    Bump copyright year in about dialog.

--- internal/widget/about-dialog.go
+++ internal/widget/about-dialog.go
@@ -19,7 +19,7 @@ func NewAboutDialog(windowRemover func(gtk.IWindow)) (*gtk.AboutDialog, error) {
 	dialog.SetComments(l("A simple xkcd viewer written in Go using GTK+3"))
 	dialog.SetWebsite("https://github.com/rkoesters/xkcd-gtk")
 	dialog.SetAuthors([]string{"Ryan Koesters"})
-	dialog.SetCopyright("Copyright © 2015-2022 Ryan Koesters")
+	dialog.SetCopyright("Copyright © 2015-2023 Ryan Koesters")
 	dialog.SetLicenseType(gtk.LICENSE_GPL_3_0)
 
 	// We want to keep the about dialog around in case we want to show it again,
