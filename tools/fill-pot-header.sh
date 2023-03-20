6da05b4dde704af7af663be9a57fdd73d0d0d8b3
Ryan Koesters <rmkoesters@gmail.com>
2023-03-19 20:24:53 -0700

    Update copyright year in tools/fill-pot-header.sh.

--- tools/fill-pot-header.sh
+++ tools/fill-pot-header.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sed \
   -e "s/SOME DESCRIPTIVE TITLE/Translations for Comic Sticks (xkcd-gtk)/" \
-  -e "s/YEAR THE PACKAGE'S COPYRIGHT HOLDER/2015-2022 Ryan Koesters/" \
+  -e "s/YEAR THE PACKAGE'S COPYRIGHT HOLDER/2015-2023 Ryan Koesters/" \
   -e 's#"Report-Msgid-Bugs-To: .*\\n"#"Report-Msgid-Bugs-To: https://github.com/rkoesters/xkcd-gtk/issues\\n"#g' \
   -e '/^"POT-Creation-Date: .*\\n"$/d' \
   -e "s/CHARSET/UTF-8/"
