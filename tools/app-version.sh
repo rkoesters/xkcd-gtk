f596181fd34555a59262c87a9fc9fd704dc19f1c
Ryan Koesters <rmkoesters@gmail.com>
2023-04-10 20:19:40 -0700

    Use functions in app-version.sh.

--- tools/app-version.sh
+++ tools/app-version.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -eu -o pipefail
 
-git describe --tags --match='v[0-9].[0-9]*.[0-9]*' --dirty 2>/dev/null ||
-grep '<release version="' data/com.github.rkoesters.xkcd-gtk.appdata.xml |
-  head -n 1 | cut -d '"' -f 2 ||
+version_from_git () {
+  git describe --tags --match='v[0-9].[0-9]*.[0-9]*' --dirty
+}
+
+version_from_appdata () {
+  grep '<release version="' data/com.github.rkoesters.xkcd-gtk.appdata.xml |
+  head -n 1 |
+  cut -d '"' -f 2
+}
+
+version_from_git 2>/dev/null ||
+version_from_appdata 2>/dev/null ||
 echo "unknown"
