272569992ba4916a868a53543c9e8e468ed09e4c
Ryan Koesters <rmkoesters@gmail.com>
2023-10-08 13:20:52 -0700

    Add gdk_pixbuf tag to pkg-config-tags.sh.

--- tools/pkg-config-tags.sh
+++ tools/pkg-config-tags.sh
@@ -9,6 +9,7 @@ pkg_config_tag () {
 }
 
 pkg_config_tag cairo 'cairo'
+pkg_config_tag gdk_pixbuf 'gdk-pixbuf-2.0'
 pkg_config_tag glib 'glib-2.0'
 pkg_config_tag gtk 'gtk+-3.0'
 pkg_config_tag pango 'pango'
