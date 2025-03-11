0006ad465fab69c37e88ca851341d8336cf8f72e
Ryan Koesters <rmkoesters@gmail.com>
2025-03-11 00:17:02 -0700

    Fix fill-pot-header.sh to not re-add copyright year.

--- tools/fill-pot-header.sh
+++ tools/fill-pot-header.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sed \
   -e "s/SOME DESCRIPTIVE TITLE/Translations for Comic Sticks (xkcd-gtk)/" \
-  -e "s/YEAR THE PACKAGE'S COPYRIGHT HOLDER/2015-2023 Ryan Koesters/" \
+  -e "/YEAR THE PACKAGE'S COPYRIGHT HOLDER/d" \
   -e 's#"Report-Msgid-Bugs-To: .*\\n"#"Report-Msgid-Bugs-To: https://github.com/rkoesters/xkcd-gtk/issues\\n"#g' \
-  -e '/^"POT-Creation-Date: .*\\n"$/d' \
+  -e '/POT-Creation-Date:/d' \
   -e "s/CHARSET/UTF-8/"
